c115c7100370ea3740b918c829da0dca1be1f0af
realnickshatskiy <real.nickshatskiy@gmail.com>
2021-10-03 16:26:39 -0400

    Revert "created file Car in Master branch"
    This reverts commit 4d15366172f92411cd7991833c026ec841d5d53c.

--- src/setup/Car.java
+++ src/setup/Car.java
@@ -1,17 +1,19 @@
 package setup;
 
 public class Car {
+<<<<<<< HEAD
 
     // This is Car file in master branch
 
     // car is Mustang GT
 
     // some text here
-
+=======
     // this is our car from James branch
 
     // car is a Tesla
 
     // some more text
 
+>>>>>>> James
 }
