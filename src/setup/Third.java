37e8bd0311c8946d50aae7f00c798531da0be2c7
realnickshatskiy <real.nickshatskiy@gmail.com>
2021-10-03 12:44:24 -0400

    created file Third.java in TC002

---
+++ src/setup/Third.java
@@ -0,0 +1,11 @@
+package setup;
+
+public class Third {
+
+    public static void main(String[] args) {
+
+        int i = 10;
+        double b = 22.2;
+        String s = "just some text";
+    }
+}
