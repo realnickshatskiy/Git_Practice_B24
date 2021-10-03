347757ae1d0e50192a486aaf8e4e12024d0dfa59
realnickshatskiy <real.nickshatskiy@gmail.com>
2021-10-03 11:31:40 -0400

    added few lines in first.j Added few lines if first.java

--- src/setup/first.java
+++ src/setup/first.java
@@ -3,5 +3,14 @@ package setup;
 public class first {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        // This is a new line
+
+        //I want to create a history
+
+        //That's it
+
+
+
     }
 }
