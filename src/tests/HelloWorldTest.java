50f090c8f19832d6fe3ed2a632a2418f9aa69205
realnickshatskiy <real.nickshatskiy@gmail.com>
2021-10-03 16:51:49 -0400

    updated HelloWorldTest class

--- src/tests/HelloWorldTest.java
+++ src/tests/HelloWorldTest.java
@@ -3,5 +3,8 @@ package tests;
 public class HelloWorldTest {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        System.out.println("One more Hello World");
+
     }
 }
